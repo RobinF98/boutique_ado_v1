5fc85bc767166224164a1abc6cb2e9e6d5861eaf
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-04-20 14:05:57 +0100

    Add class methods to wh handler and view

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -32,6 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '8000-codeanywhere-templates-p-s0p9aj3dxj.us1.codeanyapp.com',
     '127.0.0.1',
+    'localhost',
     ]
 
 
@@ -193,3 +194,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
