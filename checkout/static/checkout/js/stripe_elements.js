fbea8bafcf22194ab5325d59bdf4fa39b6688bcc
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-04-21 13:22:49 +0100

    Add checkout data caching in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -54,10 +54,45 @@ form.addEventListener('submit', function(ev) {
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
 
+    let saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // From {% csrf_token %}
+    let csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    let postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    let url = '/checkout/cache_checkout_data/';
+    console.log(postData)
+    $.post(url, postData).done(function() {
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
-            }
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address:{
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
+                    },
+                },
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
+                }
+            },
         }).then(function(result) {
             if (result.error) {
                 let errorDiv = document.getElementById('card-errors');
@@ -74,8 +109,11 @@ form.addEventListener('submit', function(ev) {
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
                     form.submit();
-                    console.log("success!!!!!!!")
                 }
             }
         });
+    }).fail(function () {
+        // Reload page, error message will be in django messages
+        location.reload();
     })
+})
