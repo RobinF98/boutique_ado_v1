6cc6d08459655cd58b9e569d5a03893b549dc364
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-05-14 22:13:28 +0100

    Add delete product functionality

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.db.models import Q
 from django.db.models.functions import Lower
 
@@ -80,9 +80,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse_lazy('product_detail', product.id))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -117,3 +117,11 @@ def edit_product(request, product_id):
         'product': product,
     }
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """Delete a product from the store"""
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted')
+    return redirect(reverse('products'))
